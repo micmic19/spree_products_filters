88c050cb3c1682f78e074f47ba947cf1d6433137
micmic19 <45197167+micmic19@users.noreply.github.com>
2019-10-01 15:35:26 +0300

    params.to_unsafe_h.merge(...)

--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -5,7 +5,7 @@ Spree::TaxonsController.class_eval do
     @taxon = Spree::Taxon.friendly.find(params[:id])
     return unless @taxon
 
-    @searcher   = build_searcher(params.merge(taxon: @taxon.id, include_images: true))
+    @searcher   = build_searcher(params.to_unsafe_h.merge(taxon: @taxon.id, include_images: true))
     @products   = @searcher.custom_retrieve_products
     @taxonomies = Spree::Taxonomy.includes(root: :children)
 
