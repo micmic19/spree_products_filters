f33aa0288aa6e8d9482da333b0f63e345ce3e76a
micmic19 <45197167+micmic19@users.noreply.github.com>
2019-10-02 11:42:17 +0300

    bug1

--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -5,15 +5,10 @@ Spree::TaxonsController.class_eval do
     @taxon = Spree::Taxon.friendly.find(params[:id])
     return unless @taxon
 
-    @searcher   = build_searcher(taxxon_params.to_h.merge(taxon: @taxon.id, include_images: true))
+    @searcher   = build_searcher(params.merge(taxon: @taxon.id, include_images: true).permit(:taxon, :include_images))
     @products   = @searcher.custom_retrieve_products
     @taxonomies = Spree::Taxonomy.includes(root: :children)
 
     filters_init
-  end
-
-  private
-  def taxxon_params
-    params.require(:taxon).permit(:taxon, :include_images)
   end
 end
