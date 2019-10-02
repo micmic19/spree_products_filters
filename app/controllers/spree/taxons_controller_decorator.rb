3532410df0886848b4d2efff01777d42d8b7580d
micmic19 <45197167+micmic19@users.noreply.github.com>
2019-10-02 11:24:57 +0300

    taxon_controller strong params

--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -5,10 +5,15 @@ Spree::TaxonsController.class_eval do
     @taxon = Spree::Taxon.friendly.find(params[:id])
     return unless @taxon
 
-    @searcher   = build_searcher(params.merge(taxon: @taxon.id, include_images: true).permit(:taxon, :include_images))
+    @searcher   = build_searcher(taxxon_params.to_h.merge(taxon: @taxon.id, include_images: true))
     @products   = @searcher.custom_retrieve_products
     @taxonomies = Spree::Taxonomy.includes(root: :children)
 
     filters_init
+  end
+
+  private
+  def taxxon_params
+    params.require(:taxon).permit(taxon:, :include_images)
   end
 end
