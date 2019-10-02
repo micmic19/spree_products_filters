5d4fe6e2744cc17871be3170ec3661469714867a
micmic19 <45197167+micmic19@users.noreply.github.com>
2019-10-02 17:47:50 +0300

    more params permit

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -2,7 +2,7 @@ Spree::ProductsController.class_eval do
   include FiltersInitialization
 
   def index
-    @searcher   = build_searcher(params.permit(:include_images).merge(include_images: true))
+    @searcher   = build_searcher(params.permit(:include_images, :sorting).merge(include_images: true))
     @products   = @searcher.custom_retrieve_products
     @taxonomies = Spree::Taxonomy.includes(root: :children)
 
