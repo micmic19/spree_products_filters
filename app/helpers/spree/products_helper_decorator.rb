2992525d9c8442ad0c7dea021d09ce99672ea435
micmic19 <45197167+micmic19@users.noreply.github.com>
2019-10-03 11:55:27 +0300

    param filters as array of array #8

--- app/helpers/spree/products_helper_decorator.rb
+++ app/helpers/spree/products_helper_decorator.rb
@@ -15,7 +15,16 @@ Spree::ProductsHelper.class_eval do
     # params.permit!
     # link_to Spree.t(key).html_safe, params.permit({ :filters => [[[]]] }, { :filters => [[]] },  :types, :filters, { :filters => [] }, :commit, :sorting, :utf8, :page).merge(sorting: key), class: klass
     # link_to Spree.t(key).html_safe, params.merge(sorting: key), class: klass
-    link_to Spree.t(key).html_safe, params.permit(:types, :filters, :commit, :sorting, :utf8, :page, filters: [ ], :filters: [ properties: [ ]], :filters: [ :properties: [:types[]]]).merge(sorting: key), class: klass
+    link_to Spree.t(key).html_safe, params.permit(:types, :filters, :commit, :sorting, :utf8, :page,
+filters: [
+  :max_price_range,
+  :min_price_range,
+  { properties: [
+    :types
+  ]
+ }
+]
+      ).merge(sorting: key), class: klass
   end
 
   def current_sorting?(key)
